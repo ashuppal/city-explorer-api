ab88aa0aabaa9248c148cb49d7889efe82b283c0
Ash Uppal <ashuppal@gmail.com>
2022-12-09 18:44:27 -0800

    lab 10

--- Movies.js
+++ Movies.js
@@ -1,3 +1,5 @@
+
+
 //code for lab 10
 
 'use strict';
@@ -58,3 +60,48 @@ class Movie {
 
 
 module.exports = getMovies;
+
+
+
+
+
+// 'use strict';
+
+// const MOVIE_API_KEY  = process.env.MOVIE_API_KEY;
+// const axios = require('axios');
+
+// async function getMovies(request,response,next) {
+
+//   let city = request.query.city;
+
+//   let url = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${city}`;
+
+// //lab 9
+//   // Make a request to the Movies API and throw an error if something goes wrong.
+//   try {
+//     let movieResponse = await axios.get(url);
+
+//     let movieData = movieResponse.data.results;
+//     let movieArray = movieData.map(movie => {
+//       return new Movie(movie);
+//     });
+
+//     response.send(movieArray);
+//   } catch (error) {
+//     next(error);
+//   }
+// }
+// class Movie {
+//   constructor(movie) {
+//     this.title = movie.title;
+//     this.overview = movie.overview;
+//     this.average_votes = movie.vote_average;
+//     this.total_votes = movie.vote_count;
+//     this.image_url = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+//     this.popularity = movie.popularity;
+//     this.released_on = movie.release_date;
+//   }
+// }
+
+
+// module.exports = getMovies;
